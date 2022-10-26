54b3a05fa957a7bfbd985160ffb23466a37dde60
MAKSY <MAKSY@DESKTOP-6S4ESUP>
2022-10-26 20:34:41 +0200

    Add project files.

---
+++ Uppdrag 4.23/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Skriv in ett heltal");
+
+int tal = int.Parse(Console.ReadLine());
+int st = tal; 
+
+string svar = " ";
+while (svar != "n")
+{
+    Console.WriteLine("Vill du skirva ett till tall eller inte (j/n)");
+    svar = Console.ReadLine();
+    if (svar == "n")
+    {
+        break;
+    }
+    Console.WriteLine("Skriv in ett heltal");
+    int ntal = int.Parse(Console.ReadLine());
+
+    if (ntal > st)
+    {
+        st = ntal;
+    }
+
+}
+Console.WriteLine($"det här var det största talet som du skerv {st}");
